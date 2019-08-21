814a6cae11afc3b73889a1fa550aeeae2414a214
pankhuri-52 <pankhuritrikha@gmail.com>
2019-08-21 21:28:13 +0530

    My own header file which executes printf function

---
+++ trial.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "printf.h"
+int main(){
+Myprintf("Hi, Everyone.. This is a trial for my own header file\n");
+int a=5;
+int b=10;
+Myprintf("%d\n",(a*b));
+return 0;
+}
